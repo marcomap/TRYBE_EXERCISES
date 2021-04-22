e53003ba4813f5a11473029ef520e944908b7def
Marco Antonio <marcoantoniocdspy@gmail.com>
2021-04-22 19:07:52 -0300

    Adiciona exercicio 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio1.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 10;
+
+soma = (a + b)
+console.log(soma)
+
+subtração = (a - b)
+console.log(subtração)
+
+multiplicação = (a * b)
+console.log(multiplicação)
+
+divisão = (a / b)
+console.log(divisão)
+
+módulo =(a % b)
+console.log(módulo)
