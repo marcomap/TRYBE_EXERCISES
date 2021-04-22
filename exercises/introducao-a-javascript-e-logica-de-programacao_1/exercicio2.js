eeb4caf1aa171924660c0836452d5399a2ef408e
Marco Antonio <marcoantoniocdspy@gmail.com>
2021-04-22 19:18:55 -0300

    Adiciona exercicio 2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio2.js
@@ -0,0 +1,10 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+let number1 = 18
+let number2 = 5
+
+if (number1>number2) {
+    console.log(number1)
+}
+else {
+    console.log(number2)
+}
