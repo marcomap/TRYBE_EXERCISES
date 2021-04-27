e5efc3e37a67c66e43d8bf8f60f0e81ccbf137f8
Marco Antonio <marcoantoniocdspy@gmail.com>
2021-04-27 15:56:48 -0300

    Adiciona exercicios 1,2,3 incompleto

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio1.js
@@ -0,0 +1,25 @@
+// Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+// let numero = 5;
+// let asterisco = '*';
+// let empty = '';
+
+// for (let index= 0; index < numero; index +=1) {
+//     empty = empty + asterisco;
+// };
+
+// for (let index= 0; index < numero; index +=1) {
+//     console.log (empty)
+// };
+
+let numero = 4;
+let simbolo = '*';
+let vazia = '';
+
+for (let index = 0; index < numero; index += 1) {
+    vazia = vazia + simbolo
+}
+for (let index = 0; index < numero; index += 1) {
+    console.log (vazia) }
+
+
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio3.js
@@ -0,0 +1,4 @@
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+// let inputPosition = n;
